7602ea26677d381ef464f707ea22a2beb2329e59
JulliaZarubina <91394502+JulliaZarubina@users.noreply.github.com>
2022-01-24 22:40:43 +0300

    Delete SecurityInitializer.java

--- src/main/java/web/config/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package web.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    //пустой класс, использующийся для регистрации модуля в спринг-контейнере
-}
